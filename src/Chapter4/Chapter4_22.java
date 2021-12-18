ee39f640a49335818915442688adfa21940cb5ad
subash <readwrite1217@gmail.com>
2021-12-18 14:24:19 -0600

    changes

--- src/Chapter4/Chapter4_22.java
+++ src/Chapter4/Chapter4_22.java
@@ -2,6 +2,7 @@ package Chapter4;
 
 public class Chapter4_22 {
     public static void main(String[] args) {
+        //need to study fomatting further
         System.out.println("N   10*N    100*N   1000*N");
         for (int i = 1; i <= 5; i++) {
             System.out.printf("%d   %d  %d  %d\n", i, 10 * i, 100 * i, 1000 * i);
