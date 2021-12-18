9b460e22a1abd3e9b61f6446d7c717c779f830ea
subash <readwrite1217@gmail.com>
2021-12-18 14:23:54 -0600

    changes

--- src/Chapter4/Chapter4_22.java
+++ src/Chapter4/Chapter4_22.java
@@ -2,9 +2,9 @@ package Chapter4;
 
 public class Chapter4_22 {
     public static void main(String[] args) {
-        System.out.println("N\t10*N\t100*N\t1000*N");
+        System.out.println("N   10*N    100*N   1000*N");
         for (int i = 1; i <= 5; i++) {
-            System.out.printf("%d\t%4d\t%4d\t%4d\n", i, 10 * i, 100 * i, 1000 * i);
+            System.out.printf("%d   %d  %d  %d\n", i, 10 * i, 100 * i, 1000 * i);
         }
     }
 }
